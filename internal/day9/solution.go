43c64cacdb905a613a52ce2ab5d32f4ed29a37dd
Alex Haynes <alexhaynes93@gmail.com>
2020-12-10 13:49:55 -0600

    Day 9 Part 1 Solution

---
+++ internal/day9/solution.go
@@ -0,0 +1,49 @@
+package day9
+
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"advent/internal/input"
+)
+
+var ErrNoInvalid = errors.New("No number found that wasn't a valid sum")
+var ErrInputTooShort = errors.New("The provided numbers slice was not longer than the given preamble size")
+
+const preambleLength = 25
+
+func Solve() {
+	data := input.NewInput("./input/day9.txt").AsInts()
+	firstInvalid, err := findFirstInvalid(preambleLength, data) //Change 5 to 25
+	if err != nil {
+		log.Fatalf("Error finding first invalid entry: %s", err)
+	}
+	fmt.Println("Part 1 Solution was", data[firstInvalid])
+}
+
+//findFirstInvalid returns the index of the first value in numbers which is not a result of summing any two discrete values of the previous preambleLength values of numbers.
+func findFirstInvalid(preambleLength int, numbers []int) (index int, err error) {
+	if !(len(numbers) > preambleLength) {
+		return 0, ErrInputTooShort
+	}
+	for i := preambleLength; i < len(numbers); i++ {
+		if !isSumOfDiscreteNPrevious(numbers[i], numbers[i-preambleLength:i]) {
+			return i, nil
+		}
+	}
+	return 0, ErrNoInvalid
+}
+
+func isSumOfDiscreteNPrevious(target int, operands []int) bool {
+	//This must check each combination without allowing them to match themselves, so the last index has to skip right?
+	//compare operands[0] + operands[1] == target, compare operands[0] + operands[len(operands)-1] == target, compare operands[1] + operands[2] == target, compare operands[len(operands)-2] + operands(len(operands)-1] == target
+	for i := 0; i < len(operands)-1; i++ { //i < len(operands) - 1 because I want to run the last loop with one unchecked pair.
+		for j := i + 1; j < len(operands); j++ {
+			if operands[i]+operands[j] == target {
+				return true
+			}
+		}
+	}
+	return false
+}
--- internal/input/read.go
+++ internal/input/read.go
@@ -1,21 +1,22 @@
 package input
 
 import (
-  "bufio"
+	"bufio"
 	"log"
 	"os"
+	"strconv"
 )
 
 type Input struct {
-  filepath string
+	filepath string
 }
 
-func NewInput(file string) (*Input) {
- return &Input{filepath: file,}
+func NewInput(file string) *Input {
+	return &Input{filepath: file}
 }
 
 func (i *Input) AsStrings() []string {
-  file, err := os.Open(i.filepath)
+	file, err := os.Open(i.filepath)
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
 	}
@@ -26,5 +27,18 @@ func (i *Input) AsStrings() []string {
 		txtlines = append(txtlines, scanner.Text())
 	}
 	file.Close()
-  return txtlines
+	return txtlines
+}
+
+func (i *Input) AsInts() []int {
+	strs := i.AsStrings()
+	var result []int
+	for _, str := range strs {
+		val, err := strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			log.Fatalf("Error converting string %s to int:%s", str, err)
+		}
+		result = append(result, int(val))
+	}
+	return result
 }
